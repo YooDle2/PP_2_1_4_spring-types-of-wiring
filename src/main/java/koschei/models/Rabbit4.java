5d97c50271eead99460b27be52d856b5f557bcf5
Ruslan <ruslan.mirzaev.work@gmail.com>
2022-11-07 12:53:05 +0300

    second

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class Rabbit4 {
     private Duck5 duck;
     @Autowired
-    public Rabbit4(Duck5 duck) {
+    public void setDuck(Duck5 duck) {
         this.duck = duck;
     }
     @Override
